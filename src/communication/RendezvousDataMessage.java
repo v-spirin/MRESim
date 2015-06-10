7ba186bda6360cf465ba8c52ced73a96bca24872
vspirin <victor.spirin@cs.ox.ac.uk>
2015-06-10 12:23:13 +0100

    Fixed license headers

--- src/communication/RendezvousDataMessage.java
+++ src/communication/RendezvousDataMessage.java
@@ -1,7 +1,42 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *     Copyright 2010, 2015 Julian de Hoog (julian@dehoog.ca), Victor Spirin (victor.spirin@cs.ox.ac.uk)
+ *
+ *     This file is part of MRESim 2.2, a simulator for testing the behaviour
+ *     of multiple robots exploring unknown environments.
+ *
+ *     If you use MRESim, I would appreciate an acknowledgement and/or a citation
+ *     of our papers:
+ *
+ *     @inproceedings{deHoog2009,
+ *         title = "Role-Based Autonomous Multi-Robot Exploration",
+ *         author = "Julian de Hoog, Stephen Cameron and Arnoud Visser",
+ *         year = "2009",
+ *         booktitle = "International Conference on Advanced Cognitive Technologies and Applications (COGNITIVE)",
+ *         location = "Athens, Greece",
+ *         month = "November",
+ *     }
+ *
+ *     @incollection{spirin2015mresim,
+ *       title={MRESim, a Multi-robot Exploration Simulator for the Rescue Simulation League},
+ *       author={Spirin, Victor and de Hoog, Julian and Visser, Arnoud and Cameron, Stephen},
+ *       booktitle={RoboCup 2014: Robot World Cup XVIII},
+ *       pages={106--117},
+ *       year={2015},
+ *       publisher={Springer}
+ *     }
+ *
+ *     MRESim is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     MRESim is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License along with MRESim.
+ *     If not, see <http://www.gnu.org/licenses/>.
  */
 package communication;
 
--- src/exploration/UtilityExploration.java
+++ src/exploration/UtilityExploration.java
@@ -1,11 +1,11 @@
 /*
- *     Copyright 2010, 2014 Julian de Hoog (julian@dehoog.ca), Victor Spirin (victor.spirin@cs.ox.ac.uk)
+ *     Copyright 2010, 2015 Julian de Hoog (julian@dehoog.ca), Victor Spirin (victor.spirin@cs.ox.ac.uk)
  *
  *     This file is part of MRESim 2.2, a simulator for testing the behaviour
  *     of multiple robots exploring unknown environments.
  *
  *     If you use MRESim, I would appreciate an acknowledgement and/or a citation
- *     of our paper:
+ *     of our papers:
  *
  *     @inproceedings{deHoog2009,
  *         title = "Role-Based Autonomous Multi-Robot Exploration",
@@ -14,6 +14,15 @@
  *         booktitle = "International Conference on Advanced Cognitive Technologies and Applications (COGNITIVE)",
  *         location = "Athens, Greece",
  *         month = "November",
+ *     }
+ *
+ *     @incollection{spirin2015mresim,
+ *       title={MRESim, a Multi-robot Exploration Simulator for the Rescue Simulation League},
+ *       author={Spirin, Victor and de Hoog, Julian and Visser, Arnoud and Cameron, Stephen},
+ *       booktitle={RoboCup 2014: Robot World Cup XVIII},
+ *       pages={106--117},
+ *       year={2015},
+ *       publisher={Springer}
  *     }
  *
  *     MRESim is free software: you can redistribute it and/or modify
